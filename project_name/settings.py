38b2b7db673f60e3b394afa5350e9ace05f7e87f
Nikita Hismatov <hismatovns@gmail.com>
2012-06-19 15:50:01 +0600

    database settings

--- project_name/settings.py
+++ project_name/settings.py
@@ -14,6 +14,17 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '{{project_name}}.db',   # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
 SECRET_KEY = '{{ secret_key }}'
 
 # Local time zone for this installation. Choices can be found here:
